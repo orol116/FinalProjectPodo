f1fcbbfee56b90ae83d47134ba45e1294e342d04
주빈 <orol116@naver.com>
2022-07-07 17:46:59 +0900

    회원 정보 수정 완료

--- PODO/src/main/java/edu/kh/podo/member/model/service/MyPageService.java
+++ PODO/src/main/java/edu/kh/podo/member/model/service/MyPageService.java
@@ -2,6 +2,8 @@ package edu.kh.podo.member.model.service;
 
 import java.util.Map;
 
+import edu.kh.podo.member.model.vo.Member;
+
 public interface MyPageService {
 
 	/** 회원 정보 수정 Service
@@ -9,5 +11,17 @@ public interface MyPageService {
 	 * @return result
 	 */
 	int updateInfo(Map<String, Object> paramMap);
+
+	/** 회원 비밀번호 수정 Service
+	 * @param pwMap
+	 * @return result
+	 */
+	int updatePw(Map<String, Object> pwMap);
+
+	/** 회원 탈퇴 Service
+	 * @param loginMember
+	 * @return result
+	 */
+	int secession(Member loginMember);
 
 }
--- PODO/src/main/java/edu/kh/podo/member/model/service/MyPageServiceImpl.java
+++ PODO/src/main/java/edu/kh/podo/member/model/service/MyPageServiceImpl.java
@@ -3,21 +3,52 @@ package edu.kh.podo.member.model.service;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import edu.kh.podo.member.model.dao.MyPageDAO;
+import edu.kh.podo.member.model.vo.Member;
 
 @Service
 public class MyPageServiceImpl implements MyPageService {
 	
 	@Autowired
 	private MyPageDAO dao;
+	
+	private BCryptPasswordEncoder bcrypt;
 
 	// 회원 정보 수정 Service 구현
 	@Override 
 	public int updateInfo(Map<String, Object> paramMap) {
 		return dao.updateInfo(paramMap);
 	}
+
+	// 회원 비밀번호 수정 Service 구현
+	@Override
+	public int updatePw(Map<String, Object> pwMap) {
+		String nowPw = dao.selectCurrentPw((int)pwMap.get("memberPw"));
+				
+		if (bcrypt.matches((String)pwMap.get("currentPw"), nowPw)) {
+			pwMap.put("newPw", bcrypt.encode((String)pwMap.get("newPw")));
+			return dao.updatePw(pwMap);
+		}
+		return 0;
+	}
+
+	// 회원 탈퇴 Service 구현
+	@Override
+	public int secession(Member loginMember) {
+		
+		String nowPw = dao.selectCurrentPw(loginMember.getMemberNo());
+		
+		if (bcrypt.matches(loginMember.getMemberPw(), nowPw)) return dao.secession(loginMember.getMemberNo());
+		
+		return 0;
+	}
+	
+	
+	
+	
  
 	
 	
