9d65976c34c767c45c77e46dedd1070068ad5bea
주빈 <orol116@naver.com>
2022-07-11 11:22:05 +0900

    MemberController

--- PODO/src/main/java/edu/kh/podo/member/controller/MemberController.java
+++ PODO/src/main/java/edu/kh/podo/member/controller/MemberController.java
@@ -31,81 +31,73 @@ public class MemberController {
 	@Autowired
 	private MemberService service;
 
-	
 	// 로그인 화면전환
-	   @GetMapping("/login")
-	   public String login() {
-	      return "/member/member-login";
-	   }
-	   
-	   // 로그인
-	   @PostMapping("/login")
-	   public String login(@ModelAttribute Member inputMember, Model model, RedirectAttributes ra,
-	         HttpServletResponse resp, HttpServletRequest req,
-	         @RequestParam(value = "saveId", required = false) String saveId) {
+	@GetMapping("/login")
+	public String login() {
+		return "/member/member-login";
+	}
 
-	      logger.info("로그인 기능 수행됨");
+	// 로그인
+	@PostMapping("/login")
+	public String login(@ModelAttribute Member inputMember, Model model, RedirectAttributes ra,
+			HttpServletResponse resp, HttpServletRequest req,
+			@RequestParam(value = "saveId", required = false) String saveId) {
 
-	      Member loginMember = service.login(inputMember);
+		logger.info("로그인 기능 수행됨");
 
-	      String path = null;
+		Member loginMember = service.login(inputMember);
 
-	      if (loginMember != null) {
+		String path = null;
 
-	         
-	         model.addAttribute("loginMember", loginMember);
+		if (loginMember != null) {
 
-	         Cookie cookie = new Cookie("saveId", loginMember.getMemberId());
+			model.addAttribute("loginMember", loginMember);
 
-	         if (saveId != null) {
+			Cookie cookie = new Cookie("saveId", loginMember.getMemberId());
 
-	            cookie.setMaxAge(60 * 60 * 24 * 365);
+			if (saveId != null) {
 
-	         } else {
-	            cookie.setMaxAge(0);
-	         }
+				cookie.setMaxAge(60 * 60 * 24 * 365);
 
-	         cookie.setPath(req.getContextPath());
-	         
-	         resp.addCookie(cookie);
-	         path = "/";
-	         
-	      } else {
-	         path = "/member/login";
-	         ra.addFlashAttribute("message", "아이디 또는 비밀번호가 일치하지 않습니다.");
+			} else {
+				cookie.setMaxAge(0);
+			}
 
-	      }
+			cookie.setPath(req.getContextPath());
 
-	      return "redirect:" + path;
-	   }
-	   
-	   
-	   // 로그아웃
-	   @GetMapping("/logout")
-	   public String logout(SessionStatus status) {
-	      
-	      logger.info("로그아웃 수행됨");
-	      
-	      status.setComplete();
-	      
-	      return "redirect:/";
-	   }
-	   
-	   
-	   // 회원가입
-	   @GetMapping("/signUp")
-	   public String signUp() {
-	      
-	      return "/member/signUp";
-	   }
-	   
-	   
-	
-	
-		
-		// 판매상품 업로드 페이지
-		@PostMapping("/member-upload")
-		public String upload() {
-			return "member/member-upload";
+			resp.addCookie(cookie);
+			path = "/";
+
+		} else {
+			path = "/member/login";
+			ra.addFlashAttribute("message", "아이디 또는 비밀번호가 일치하지 않습니다.");
+
 		}
+
+		return "redirect:" + path;
+	}
+
+	// 로그아웃
+	@GetMapping("/logout")
+	public String logout(SessionStatus status) {
+
+		logger.info("로그아웃 수행됨");
+
+		status.setComplete();
+
+		return "redirect:/";
+	}
+
+	// 회원가입
+	@GetMapping("/signUp")
+	public String signUp() {
+
+		return "/member/signUp";
+	}
+
+	// 판매상품 업로드 페이지
+	@PostMapping("/member-upload")
+	public String upload() {
+		return "member/member-upload";
+	}
 }
