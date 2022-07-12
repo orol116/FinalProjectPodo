f1b3f4360b842e4d0c185a5b1a67162c03ec8bfa
DESKTOP-IJ8C807\user1 <testplay36@gmail.com>
2022-07-12 16:17:48 +0900

    핸드폰 본인인증(아이디 찾기)

--- PODO/src/main/java/edu/kh/podo/member/controller/MemberController.java
+++ PODO/src/main/java/edu/kh/podo/member/controller/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -219,6 +220,7 @@ public class MemberController {
 	public String fingId() {
 		return "/member/member-find-ID";
 	}
+
 
 	// 비밀번호 찾기 페이지 전환
 	@GetMapping("/findPw")
