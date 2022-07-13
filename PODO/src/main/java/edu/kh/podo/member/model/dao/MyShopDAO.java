d389227e80abfd66f74d0ef3c26e6bd09d7a40b9
주빈 <orol116@naver.com>
2022-07-13 12:47:43 +0900

    내 상점 수정중

--- PODO/src/main/java/edu/kh/podo/member/model/dao/MyShopDAO.java
+++ PODO/src/main/java/edu/kh/podo/member/model/dao/MyShopDAO.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import edu.kh.podo.board.itemBoard.model.vo.ItemBoard;
+import edu.kh.podo.member.model.vo.Member;
 import edu.kh.podo.member.model.vo.Review;
 
 @Repository
@@ -29,6 +30,14 @@ public class MyShopDAO {
 	 */
 	public List<Review> selectMemberReview(int memberNo) {
 		return sqlSession.selectList("myShopMapper.selectMemberReview", memberNo);
+	}
+
+	/** 내 상점 회원 정보 조회 DAO
+	 * @param memberNo
+	 * @return member
+	 */
+	public List<Member> selectMemberInfo(int memberNo) {
+		return sqlSession.selectList("myShopMapper.selectMemberInfo", memberNo);
 	}
 
 }
--- PODO/src/main/java/edu/kh/podo/member/model/service/MyShopService.java
+++ PODO/src/main/java/edu/kh/podo/member/model/service/MyShopService.java
@@ -3,6 +3,7 @@ package edu.kh.podo.member.model.service;
 import java.util.List;
 
 import edu.kh.podo.board.itemBoard.model.vo.ItemBoard;
+import edu.kh.podo.member.model.vo.Member;
 import edu.kh.podo.member.model.vo.Review;
 
 public interface MyShopService {
@@ -18,5 +19,11 @@ public interface MyShopService {
 	 * @return reviewList
 	 */
 	List<Review> selectMemberReview(int memberNo);
+	
+	/** 내 상점 회원 정보 조회 Service
+	 * @param memberNo
+	 * @return member
+	 */
+	List<Member> selectMemberInfo(int memberNo);
 
 }
