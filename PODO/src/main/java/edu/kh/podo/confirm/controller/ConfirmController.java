206d2a62656e98ec904527ea94dea97d76eb03c0
Leejj <zero_joon@naver.com>
2022-07-13 18:02:14 +0900

    본인인증 끝

--- PODO/src/main/java/edu/kh/podo/confirm/controller/ConfirmController.java
+++ PODO/src/main/java/edu/kh/podo/confirm/controller/ConfirmController.java
@@ -2,6 +2,8 @@ package edu.kh.podo.confirm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,13 +14,19 @@ public class ConfirmController {
 
 	private ConfirmService service;
 	
-	@GetMapping("/phoneCheck")
-	@ResponseBody
-	public String sendSMS(@RequestParam("phone-box") String userPhoneNumber) { // 휴대폰 문자보내기
-		int randomNumber = (int)((Math.random()* (9999 - 1000 + 1)) + 1000);//난수 생성
+	/*
+	 * @RequestMapping(value = "/phoneCheck", method = RequestMethod.GET)
+	 * 
+	 * @ResponseBody public String sendSMS(@RequestParam("phone-box") String
+	 * userPhoneNumber) { // �쑕���룿 臾몄옄蹂대궡湲� int randomNumber = (int)((Math.random()*
+	 * (9999 - 1000 + 1)) + 1000);//�궃�닔 �깮�꽦
+	 * 
+	 * service.certifiedPhoneNumber(userPhoneNumber,randomNumber);
+	 * 
+	 * return Integer.toString(randomNumber); }
+	 */
+	
 
-		service.certifiedPhoneNumber(userPhoneNumber,randomNumber);
-		
-		return Integer.toString(randomNumber);
-	}
+
 }
+
