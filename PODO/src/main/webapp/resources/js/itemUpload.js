c8520b7008004e049590b8a7150254b5f1d535c0
DESKTOP-IJ8C807\user1 <bono1223234@gmail.com>
2022-07-15 18:46:04 +0900

    no message

--- PODO/src/main/webapp/resources/js/itemUpload.js
+++ PODO/src/main/webapp/resources/js/itemUpload.js
@@ -99,7 +99,7 @@ function McategoryList(event){
                 const li = document.createElement("li");
                 const button =document.createElement("button");
 
-                button.innerHTML=mct.MCategoryName
+                button.innerHTML=mct.MCategoryName;
                 button.classList.add("select2");
                 
                 button.setAttribute('type','button')
@@ -122,6 +122,7 @@ function McategoryList(event){
                     console.log(event.target)
                     document.getElementsByName("mCateValue")[0].value =   event.target.getAttribute('id').substring(1);          
                     console.log(document.getElementsByName("mCateValue")[0].value);
+                    
                 } 
             }
             
@@ -144,9 +145,18 @@ function McategoryList(event){
 $('.select').on('click', function(){
   $('#category4').text($(this).text());
 });
-$('#category3').on('click', function(){
-    $('#category5').text($(this).text());
-  });
+
+
+// $(".select2").click(function(){
+//     $('#category5').text('MCate')
+// });
+
+function mCategoryClick(click){
+    const btnElement = document.getElementById('category5');
+    btnElement.innerText = 'mct.MCategoryName';
+}
+
+
 
 
 const selectList = document.getElementsByClassName("select");
